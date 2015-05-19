798c58b3e826226f95cac48048104c45ced4f48d
Garrett Fuller <garrettec1@gmail.com>
2015-05-18 17:41:22 -0700

    Adds comments, adds magit-status keybinding to C-x g

--- personal/settings.el
+++ personal/settings.el
@@ -1,14 +1,26 @@
 (setq prelude-user-init-file load-file-name)
+
+;; enables some std modes
 (global-linum-mode t)
-(prelude-require-packages '(yasnippet))
-(require 'yasnippet)
-(yas-global-mode t)
-(add-to-list 'load-path "~/.emacs.d/personal/jdee-2.4.1/lisp")
-(load "jde")
-(global-set-key (kbd "C-z") 'compile)
 (column-number-mode)
 (global-linum-mode)
 
+(prelude-require-packages '(yasnippet))
+(require 'yasnippet)
+
+(yas-global-mode t)
+
+;; commented out JDE since I am not doing it right now, and it loads every time
+;;(add-to-list 'load-path "~/.emacs.d/personal/jdee-2.4.1/lisp")
+;;(load "jde")
+
+;; KEY BINDINGS
+(global-set-key (kbd "C-z") 'compile)
+;; Magit rules!
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+;; fixes JDE lin num mode credit: Geoff Shannon
 (setq jde-mode-line-format '("-" mode-line-mule-info mode-line-modified mode-line-frame-identification mode-line-buffer-identification " " global-mode-string
                              (line-number-mode "(%l,")
                              (column-number-mode "%c)")
